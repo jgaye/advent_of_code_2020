ea8be8c5332bc3219be1c79007c5f3a0745176ca
Julien Gaye <julien@getluko.com>
2020-12-01 22:30:52 +0100

    encapsulating the complement finding method

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,29 +1,34 @@
-if __name__ == '__main__':
+import math
+from typing import List
+
+
+def look_for_complement(objective: int, candidates: List[int]):
+    for element_1 in candidates:
+        complement = objective - element_1
+        if complement in candidates[1:]:
+            return element_1, complement
+
+        # if complement is not found, element_1 can be removed from the search
+        candidates.remove(element_1)
+    return None, None
+
+
+if __name__ == "__main__":
 
     # with open("example_1", "r") as f:
     with open("puzzle_1", "r") as f:
         input_data = [int(line.rstrip()) for line in f]
 
-    part_1_input = input_data.copy()
-    for expense in part_1_input:
-        # Looking for the 2020 complement directly
-        if 2020-expense in input_data:
-            print(f"part 1 : {expense * (2020-expense)}")
+    print(f"part 1 : {math.prod(look_for_complement(2020, input_data.copy()))}")
+
+    part_2_input = input_data.copy()
+    for element_1 in part_2_input:
+        element_2, element_3 = look_for_complement(
+            2020 - element_1, part_2_input[1:].copy()
+        )
+        if element_2:
+            print(f"part 1 : {element_1*element_2*element_3}")
             break
 
         # if not found, you can remove the initial element to optimize consequent checks
-        part_1_input.remove(expense)
-
-    part_2_input = input_data.copy()
-    for expense in part_2_input:
-        for expense_2 in reversed(part_2_input):
-            if 2020 - expense - expense_2 in input_data:
-                print(f"part 2 : {expense * expense_2 * (2020 - expense - expense_2)}")
-                break
-        else:
-            # Continue if the inner loop wasn't broken.
-            # if not found, you can remove the initial element to optimize consequent checks
-            part_2_input.remove(expense)
-            continue
-        # Inner loop was broken, break the outer.
-        break
+        part_2_input.remove(element_1)
